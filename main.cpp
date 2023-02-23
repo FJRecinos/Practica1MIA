6c5ba773bdba7a1bd38c16c1e4d859ec7452c78a
Fernando Recinos <2086090400101@ingenieria.usac.edu.gt>
2023-02-22 18:02:08 -0600

    primer commit

--- main.cpp
+++ main.cpp
@@ -1,44 +1,12 @@
-#include <iostream>
+#include <string.h>
 #include <string>
+#include <iostream>
+
 using namespace std;
 
-string instruction;
-
-void WriteInstruction() {
-  cout << "Escribe una instrucción: ";
-  getline(cin, instruction);
-}
-
-void ReviewInstruction() {
-  cout << "La instrucción escrita es: " << instruction << endl;
-}
-
-int main() {
-  int choice = 0;
-  while (choice != 3) {
-    cout << "Menú:" << endl;
-    cout << "1. Escribir instrucción" << endl;
-    cout << "2. Revisar instrucción" << endl;
-    cout << "3. Salir" << endl;
-    cout << "Elige una opción: ";
-    cin >> choice;
-    cin.ignore();
-
-    switch (choice) {
-      case 1:
-        WriteInstruction();
-        break;
-      case 2:
-        ReviewInstruction();
-        break;
-      case 3:
-        cout << "Saliendo del programa." << endl;
-        break;
-      default:
-        cout << "Opción inválida." << endl;
-        break;
-    }
-  }
-
-  return 0;
+int main(int argc, char const *argv[])
+{
+    cout<<"hola mundo"<<endl;
+    
+    return 0;
 }
